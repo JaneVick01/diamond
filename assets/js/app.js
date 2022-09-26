e6d2d00b0b6561afb116ad391af29143fca5b4e3
JaneVick01 <ziak0001@hz.nl>
2022-09-26 22:07:28 +0200

    btn fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
 console.log('JavaScript is working!')
+const name = document.querySelector(".name");
+const btn = document.querySelector("button");
+
+      btn.addEventListener("click", function () {
+        name.style.color = "blue";
+ });
+
+ btn.addEventListener("click", changeColor);
+      function changeColor() {
+        name.style.color = "blue";
+}
